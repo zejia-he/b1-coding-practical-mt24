e29e481b9501964c59880532f9c00f6ffa1c058a
Zejia He <zejia.he@lincoln.ox.ac.uk>
2025-10-14 15:05:49 +0100

    Adding method that extracts data from csv file

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
 from .terrain import generate_reference_and_limits
+import pandas as pd
 
 class Submarine:
     def __init__(self):
@@ -75,8 +76,12 @@ class Mission:
 
     @classmethod
     def from_csv(cls, file_name: str):
-        # You are required to implement this method
-        pass
+        import pandas as pd
+        df = pd.read_csv(file_name)
+        reference = df['reference'].to_numpy()
+        cave_height = df['cave_height'].to_numpy()
+        cave_depth = df['cave_depth'].to_numpy()
+        return cls(reference, cave_height, cave_depth)
 
 
 class ClosedLoop:
